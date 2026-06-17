4c8a7c3e36fd9bcef3a1fc5c9b1c5f25000ac616
Slim <selmanislim8@gmail.com>
2026-06-17 13:07:02 +0200

    Day 3: CSS variables

--- 02 - JS and CSS clock/clock.js
+++ 02 - JS and CSS clock/clock.js
@@ -1,17 +1,24 @@
 const secondHand = document.querySelector(".second-hand");
 const minuteHand = document.querySelector(".min-hand");
 const hourHand = document.querySelector(".hour-hand");
+
+const getDegrees = (value, max) => (value / max) * 360 + 90;
+
 const setDate = function () {
   const now = new Date();
+
   const seconds = now.getSeconds();
   const minutes = now.getMinutes();
   const hours = now.getHours();
-  const secondsDegrees = (seconds / 60) * 360 + 90;
-  const minutesDegrees = (minutes / 60) * 360 + 90;
-  const hoursDegrees = (hours / 12) * 360 + 90;
-  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
-  minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
-  hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+
+  secondHand.style.transform = `rotate(${getDegrees(seconds, 60)}deg)`;
+
+  minuteHand.style.transform = `rotate(${getDegrees(minutes, 60)}deg)`;
+
+  hourHand.style.transform = `rotate(${
+    getDegrees(hours % 12, 12) + (minutes / 60) * 30
+  }deg)`;
 };
 
+setDate(); // initialize immediately
 setInterval(setDate, 1000);
