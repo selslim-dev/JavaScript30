59907af10956a8ef853aa03d0ea95a876d8bcf1e
Slim <selmanislim8@gmail.com>
2026-06-23 17:17:31 +0100

    Day 18: tally string times with reduce

--- 17 - Sort Without Articles/sort.js
+++ 17 - Sort Without Articles/sort.js
@@ -17,7 +17,7 @@ const bands = [
 const strip = function (bandName) {
   return bandName.replace(/^(a |the |an )/i, "").trim();
 };
-const sortBands = bands.sort((a, b) => (strip(a) > strip(b) ? 1 : -1));
-document.querySelector("#bands").innerHTML = sortBands
+const sortedBands = bands.sort((a, b) => (strip(a) > strip(b) ? 1 : -1));
+document.querySelector("#bands").innerHTML = sortedBands
   .map((band) => `<li>${band}</li>`)
   .join("");
